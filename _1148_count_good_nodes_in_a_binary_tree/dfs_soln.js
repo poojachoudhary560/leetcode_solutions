23ffcb9247a192205b4fa1f82fd2990c1d3eaad6
Pooja Choudhary <poojachoudhary560@gmail.com>
2023-03-03 08:57:19 +0530

    Q1148 Count Good Nodes In a Binary Tree

---
+++ _1148_count_good_nodes_in_a_binary_tree/dfs_soln.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var dfs = function (root, min) {
+  if (!root) return 0;
+  let ans = 0;
+  if (root.val >= min) {
+    ans++;
+    min = root.val;
+  }
+  return ans + dfs(root.left, min) + dfs(root.right, min);
+};
+var goodNodes = function (root) {
+  let min = -Infinity;
+  return dfs(root, min);
+};
