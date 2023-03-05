9af75ce646c63abe673852e4f25dc6911ab58ad1
Pooja Choudhary <poojachoudhary560@gmail.com>
2023-03-05 18:51:40 +0530

    Q1457 Pseudo-Palindromic paths in a binary tree

---
+++ _1457_pseudo-palindromic_paths_in_a_binary_tree/dfs_soln.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var dfs = function (root, count) {
+  if (!root) return 0;
+  if (root.left == null && root.right == null) {
+    let c = 0;
+    count[root.val]++;
+    // console.log(JSON.stringify([...count]));
+    for (let i = 0; i < count.length; i++) {
+      if (count[i] % 2 === 1) {
+        c++;
+      }
+    }
+    count[root.val]--;
+    if (c > 1) {
+      return 0;
+    }
+    return 1;
+  } else {
+    count[root.val]++;
+    let ans = dfs(root.left, count) + dfs(root.right, count);
+    count[root.val]--;
+    return ans;
+  }
+};
+var pseudoPalindromicPaths = function (root) {
+  let count = Array(10).fill(0);
+  return dfs(root, count);
+};
