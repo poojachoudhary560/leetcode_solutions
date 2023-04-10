f0cc8116918947a5813fa3e7cdbd4c65b9e9cf90
Pooja Choudhary <poojachoudhary560@gmail.com>
2023-04-10 11:32:27 +0530

    Q53. Maximum subarray

---
+++ _53_maximum_subarray/kadanne's_algo_soln.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let maxSum = -Infinity,
+    sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    maxSum = Math.max(sum, maxSum);
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return maxSum;
+};
